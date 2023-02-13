7e209fb86f84edebbf229fa91ba7d1589163c4c7
Sannnq <founguesflorin@gmail.com>
2023-02-14 00:08:29 +0100

    update

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,7 +4,7 @@ const Hero=()=>{
 
 return<>
 
- <div className="px-6 py-12 lg:py-24 md:px-12 text-center lg:text-left background-radial-gradient">
+ <div className="px-6 py-12 lg:py-24 md:px-12 text-center lg:text-left background-radial-gradient overflow-hidden">
       <div className="container mx-auto xl:px-32 text-gray-800">
         <div className="grid lg:grid-cols-2 gap-12 xl:flex items-center">
           <div className="mt-12 lg:mt-0" style={{zIndex:10}}>
@@ -16,9 +16,9 @@ return<>
             </p>
           </div>
           <div className="mb-12 lg:mb-0 relative">
-            {/* <div id="radius-shape-1" className="absolute rounded-full shadow-lg"></div>
-            <div id="radius-shape-2" className="absolute shadow-lg"></div> */}
-            <div className=" rounded-lg bg-glass px-6 py-12 md:px-0 lg:w-[150%]">
+            <div id="radius-shape-1" className="absolute rounded-full shadow-lg"></div>
+            <div id="radius-shape-2" className="absolute shadow-lg"></div>
+            <div className=" rounded-lg bg-glass px-6 py-12 md:px-0 -ml-20 sm:ml-16 w-[150%] lg:w-[200%] lg:m-0">
              
             <img src='mobile.png' alt=''/>
 
