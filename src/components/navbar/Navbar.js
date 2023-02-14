046fa72643056680f6a86f84c42581240c894514
Sannnq <founguesflorin@gmail.com>
2023-02-14 12:16:02 +0100

    logo and name changed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,10 @@
 import 'flowbite';
 const Navbar = () => {
-  return <><nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+  return <><nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 top-0">
     <div className="container flex flex-wrap items-center justify-between mx-auto">
       <a href="/" className="flex items-center">
-        <img src="logo.png" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Linker</span>
+        <img src="logo.png" className="h-12 mr-3 sm:h-12 lg:ml-32" alt="Flowbite Logo" />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
       </a>
       <div className="flex md:order-2">
         <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Contactez-nous</button>
