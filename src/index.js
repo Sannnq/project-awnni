b2a06e43238783fcc4eeeb1aa6fce950a16f1061
Sannnq <founguesflorin@gmail.com>
2023-02-15 13:02:29 +0100

    front update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/tailwind.css';
+import "flowbite"
 import App from './App';
 import Navbar from "./components/navbar/Navbar";
 import Hero from './components/hero/Hero';
