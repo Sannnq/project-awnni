7e7fbdd637b09cffd79082a2ca93978799583ba1
Sannnq <founguesflorin@gmail.com>
2023-02-15 19:30:17 +0100

    dark mode fixed

--- src/Points/Points.js
+++ src/Points/Points.js
@@ -4,50 +4,50 @@ return<section class="bg-gray-800 pt-20 pb-20">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div class="max-w-screen-md mb-8 lg:mb-16">
         <p class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-10" style={{color: "hsl(218, 81%, 95%)"}}>Services disponibles,</p>
-            {/* <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p> */}
+            {/* <p class=" sm:text-xl text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p> */}
         </div>
         <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full  lg:h-12 lg:w-12 0">
-                    <img class="w-10 h-10 text-blue-600 lg:w-12 lg:h-12 ml-1 dark:text-blue-300" src="icons/install.png" alt=""/>
+                    <img class="w-10 h-10  lg:w-12 lg:h-12 ml-1 text-blue-300" src="icons/install.png" alt=""/>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Service d'installation</h3>
-                <p class="text-gray-500 dark:text-gray-400 text-justify">Installation d'équipemments et d'appareils électroniques, comprenant les systèmes de réseaux et sécurités.</p>
+                <h3 class="mb-2 text-xl font-bold text-white">Service d'installation</h3>
+                <p class=" text-gray-400 text-justify">Installation d'équipemments et d'appareils électroniques, comprenant les systèmes de réseaux et sécurités.</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
-                    <img class="w-10 h-10 text-blue-600 lg:w-12 lg:h-12 ml-1 dark:text-blue-300" src="icons/maintenance.png" alt=""/>
+                    <img class="w-10 h-10  lg:w-12 lg:h-12 ml-1 text-blue-300" src="icons/maintenance.png" alt=""/>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Service de maintenance</h3>
-                <p class="text-gray-500 dark:text-gray-400 text-justify">Entretien régulier d'équipement électroniques et systèm automatiques afin d'assurer leurs fonctionnement continue et prevenir d'eventuelles défaillances.</p>
+                <h3 class="mb-2 text-xl font-bold text-white">Service de maintenance</h3>
+                <p class=" text-gray-400 text-justify">Entretien régulier d'équipement électroniques et systèm automatiques afin d'assurer leurs fonctionnement continue et prevenir d'eventuelles défaillances.</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
-                    <img class="w-10 h-10 text-blue-600 lg:w-12 lg:h-12 ml-1 dark:text-blue-300" src="icons/update.png" alt=""/>                    
+                    <img class="w-10 h-10  lg:w-12 lg:h-12 ml-1 text-blue-300" src="icons/update.png" alt=""/>                    
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Mise à niveau et modification</h3>
-                <p class="text-gray-500 dark:text-gray-400 text-justify">Mise à niveau et modifications d'équipements, d'appareils electroniques et systemes automatiques.</p>
+                <h3 class="mb-2 text-xl font-bold text-white">Mise à niveau et modification</h3>
+                <p class=" text-gray-400 text-justify">Mise à niveau et modifications d'équipements, d'appareils electroniques et systemes automatiques.</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
-                    <img class="w-10 h-10 text-blue-600 lg:w-12 lg:h-12 ml-1 dark:text-blue-300" src="icons/recycle.png" alt=""/>
+                    <img class="w-10 h-10  lg:w-12 lg:h-12 ml-1 text-blue-300" src="icons/recycle.png" alt=""/>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Recyclage et élimination</h3>
-                <p class="text-gray-500 dark:text-gray-400 text-justify">Recyclage et élimination responsable des déchets électroniques.</p>
+                <h3 class="mb-2 text-xl font-bold text-white">Recyclage et élimination</h3>
+                <p class=" text-gray-400 text-justify">Recyclage et élimination responsable des déchets électroniques.</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
-                    <img class="w-10 h-10 text-blue-600 lg:w-12 lg:h-12 ml-1 dark:text-blue-300" src="icons/assistance.png" alt=""/>
+                    <img class="w-10 h-10  lg:w-12 lg:h-12 ml-1 text-blue-300" src="icons/assistance.png" alt=""/>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Assistance technique</h3>
-                <p class="text-gray-500 dark:text-gray-400 text-justify">Mise à disposition d'une assistance techniquepour les equipement et appareils electroniques, afin de palier a tous problèmes de disponibilités.</p>
+                <h3 class="mb-2 text-xl font-bold text-white">Assistance technique</h3>
+                <p class=" text-gray-400 text-justify">Mise à disposition d'une assistance techniquepour les equipement et appareils electroniques, afin de palier a tous problèmes de disponibilités.</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12 ">
-                    <img class="w-10 h-10 text-blue-600 lg:w-12 lg:h-12 ml-1 dark:text-blue-300" src="icons/shop.png" alt=""/>
+                    <img class="w-10 h-10  lg:w-12 lg:h-12 ml-1 text-blue-300" src="icons/shop.png" alt=""/>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Vente et distribution</h3>
-                <p class="text-gray-500 dark:text-gray-400 text-justify">Service de vente et distribution d'appareils électronique, systemes automatiques et de sécurité.</p>
+                <h3 class="mb-2 text-xl font-bold text-white">Vente et distribution</h3>
+                <p class=" text-gray-400 text-justify">Service de vente et distribution d'appareils électronique, systemes automatiques et de sécurité.</p>
             </div>
         </div>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,20 @@
 const Footer=()=>{
 
-return<footer className="flex justify-center px-4 pt-10 text-gray-800 bg-gray-900">
+return<footer className="flex justify-center px-4 pt-10 text-gray-300 bg-gray-900">
     <div className="container px-6 py-6">
         <h1 className="text-lg font-bold text-center lg:text-2xl">
            Inscrivez-vous à notre Newletter, <br/> pour ne rien manquer sur nos mise a jours.
         </h1>
 
         <div className="flex flex-col justify-center mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
-            <input id="email" type="text" className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Votre adress email"/>
+            <input id="email" type="text" className="px-4 py-2  border rounded-md bg-gray-800 text-gray-300 border-gray-600  focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Votre adress email"/>
     
             <button className="w-full px-6 py-2.5 text-lg font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
                 S'inscrire
             </button>
         </div>
 
-        <hr className="h-px bg-gray-200 border-none my-7 dark:bg-gray-700"/>
+        <hr className="h-px  border-none my-7 bg-gray-700"/>
 
         <div className="flex flex-col items-center justify-between md:flex-row"/>
           
@@ -22,10 +22,10 @@ return<footer className="flex justify-center px-4 pt-10 text-gray-800 bg-gray-90
             <div className="flex justify-center mt-4 md:m-0">
                 <div className="-mx-4">
                     <span> © Copyright 2023, Linker Co. </span>
-                    {/* <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">About</a>
-                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Blog</a>
-                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">News</a>
-                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 hover:underline">Contact</a> */}
+                    {/* <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 text-gray-200 hover:text-blue-500 hover:text-blue-400 hover:underline">About</a>
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 text-gray-200 hover:text-blue-500 hover:text-blue-400 hover:underline">Blog</a>
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 text-gray-200 hover:text-blue-500 hover:text-blue-400 hover:underline">News</a>
+                    <a href="#" className="px-4 text-sm text-gray-600 transition-colors duration-300 text-gray-200 hover:text-blue-500 hover:text-blue-400 hover:underline">Contact</a> */}
                 </div>
             </div>
         </div>
