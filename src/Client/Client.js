dbf884de181783d79b3e87b15c39aee8c1f9840f
Sannnq <founguesflorin@gmail.com>
2023-02-15 13:20:52 +0100

    update

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -11,22 +11,22 @@ return<>
                     <div class="mt-16 md:ml-0 md:mr-0 lg:ml-16 lg:mr-16 xl:ml-20 xl:mr-20 2xl:ml-40 2xl:mr-40">
                         <ul class="grid grid-cols-2 gap-2 items-center content-center md:grid-cols-4 md:p-4 md:mt-8 lg:grid-cols-4 lg:gap-6 lg:p-6 lg:mt-0 lg:ml-auto lg:mr-auto xl:w-5/6 2xl:grid-cols-4 2xl:p-24 2xl:w-full">
                             <li class=" rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
-                                <p href="/" class="text-center"><span class="opacity-80 lg:text-xl font-medium text-justify">Accès à des experts qualifiés</span>
+                                <p href="/" class="text-center"><span class="opacity-80 lg:text-xl text-justify font-bold tracking-tight">Accès à des experts qualifiés</span>
                                    
                                 </p>
                             </li>
-                            <li class="rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] lg:hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
-                                <p href="/" class="text-center"><span class=" opacity-80 lg:text-xl font-medium">Gain de<br/>  temps et d'argent</span>
+                            <li class="rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
+                                <p href="/" class="text-center"><span class=" opacity-80 lg:text-xl font-bold tracking-tight">Gain de<br/>  temps et d'argent</span>
                                     
                                 </p>
                             </li>
-                            <li class="rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] lg:hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
-                                <p href="/" class="text-center"><span class="opacity-80 lg:text-xl font-medium">Meilleure relation client/expert</span>
+                            <li class="rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
+                                <p href="/" class="text-center"><span class="opacity-80 lg:text-xl font-bold tracking-tight">Meilleure relation client/expert</span>
                                    
                                 </p>
                             </li>
-                            <li class="rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] lg:hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
-                                <p href="/" class="text-center"><span class="opacity-80 lg:text-xl font-medium">Amélioration <br/>de la satisfaction</span>
+                            <li class="rounded-2xl w-42 p-8 h-42 m-1 bg-white drop-shadow-none shadow-sm text-center grid content-center md:h-48 lg:m-0 lg:w-4/4 lg:relative lg:hover:-translate-y-5  lg:transition-all hover:bg-[#2563EB] hover:text-white 2xl:m-0 2xl:w-full 2xl:relative 2xl:h-72 2xl:hover:-translate-y-5  2xl:transition-all 2xl:hover:bg-violet-maadana 2xl:hover:text-white">
+                                <p href="/" class="text-center"><span class="opacity-80 lg:text-xl font-bold tracking-tight">Amélioration <br/>de la satisfaction</span>
                                 
                                 </p>
                             </li>
